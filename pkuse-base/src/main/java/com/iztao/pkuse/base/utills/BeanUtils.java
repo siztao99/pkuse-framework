0d175a3283a810c7e1ca75b525840deacc1a0de2
siztao_99 <siztao_99@hotmail.com>
2018-12-18 14:22:00 +0800

    Default Changelist

---
+++ pkuse-base/src/main/java/com/iztao/pkuse/base/utills/BeanUtils.java
@@ -0,0 +1,75 @@
+package com.iztao.pkuse.base.utills;
+
+import com.google.common.collect.Maps;
+
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+/**
+ *  @author  siztao
+ *  @date     2018年 12 月 18日 下午 13:04
+ *  @description bean map 转换类
+ */
+public class BeanUtils {
+
+    public BeanUtils()
+    {
+    }
+
+    public static Map<String, Object> objectToMap(Object obj) throws Exception {
+        if(obj == null)
+            return null;
+
+        Map<String, Object> map = Maps.newConcurrentMap();
+
+        BeanInfo beanInfo = Introspector.getBeanInfo(obj.getClass());
+        PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+        for (PropertyDescriptor property : propertyDescriptors) {
+            String key = property.getName();
+            if (key.compareToIgnoreCase("class") == 0) {
+                continue;
+            }
+            Method getter = property.getReadMethod();
+            Object value = getter!=null ? getter.invoke(obj) : null;
+            map.put(key, value);
+        }
+
+        return map;
+    }
+
+    /**
+     * 建议用使用reflect进行转换，因为使用Introspector进行转换 有问题
+     * @param map
+     * @param beanClass
+     * @return
+     * @throws Exception
+     */
+    public static Object mapToObject(Map<String, Object> map, Class<?> beanClass) throws Exception {
+
+		/*if (map == null)
+            return null;
+
+        Object obj = beanClass.newInstance();
+
+        BeanInfo beanInfo = Introspector.getBeanInfo(obj.getClass());
+        PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+        for (PropertyDescriptor property : propertyDescriptors) {
+            Method setter = property.getWriteMethod();
+            if (setter != null) {
+                setter.invoke(obj, map.get(property.getName()));
+            }
+        }*/
+
+        if (map == null)
+            return null;
+
+        Object obj = beanClass.newInstance();
+
+        org.apache.commons.beanutils.BeanUtils.populate(obj, map);
+
+        return obj;
+    }
+}
